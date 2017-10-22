91ec4ef4e1602e4500b582946d1cf79890035ed5
UncleFreddy <gnlipatov@gmail.com>
2017-10-22 23:20:31 +0300

    vse

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,14 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var minify = require('gulp-csso');
+var rename = require("gulp-rename");
+var imagemin = require('gulp-imagemin');
+var svgstore = require('gulp-svgstore');
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var run = require('run-sequence');
+var del = require('del');
 
 gulp.task("style", function() {
   gulp.src("sass/*.scss")
@@ -15,7 +23,66 @@ gulp.task("style", function() {
       autoprefixer()
     ]))
     .pipe(gulp.dest("css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"))
     .pipe(server.stream());
+});
+
+gulp.task("images", function () {
+  return gulp.src("img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.svgo ()
+    ]))
+    .pipe(gulp.dest("img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg:true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("img"));
+});
+
+gulp.task("posthtml", function () {
+  return gulp.src("img/icon-*.svg")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("."));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("copy", function(){
+  return gulp.src([
+    "fonts/**/*.{woff, woff2}",
+    "img/**",
+    "js/**",
+    "css/style.min.css",
+    "index.html",
+    "catalog.html",
+    "cart.html"
+  ], {
+    base: "."
+  })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", function (done) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "sprite",
+    "posthtml",
+    done);
 });
 
 gulp.task("serve", ["style"], function() {
